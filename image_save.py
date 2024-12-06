3118f88bc36a90d00a0c626dc6d7a51d9a51c447
JB-Bai <1355864570@qq.com>
2024-12-06 18:08:40 +0800

    update

--- image_save.py
+++ image_save.py
@@ -18,10 +18,10 @@ print("Saved input image as 'input_image.jpg'.")
 
 # Save the mask image (MASK_IMG)
 mask_img = data_dict['MASK_IMG']
-mask_img.save('mask_image.png')
+mask_img.save('mask_image.png') # Note that the format of the mask image may need to be adjusted. Refer to https://github.com/viiika/HumanEdit/mask_convert.py for more details.
 print("Saved mask image as 'mask_image.png'.")
 
 # Save the output image (OUTPUT_IMG)
 output_img = data_dict['OUTPUT_IMG']
 output_img.save('output_image.jpg')
-print("Saved output image as 'output_image.jpg'.")
+print("Saved output image as 'output_image.png'.")
